67511add73938c15ba9a69f66d56d97ca9d6312b
vuongicesmile <boide03@gmail.com>
2020-08-12 22:30:04 +0700

    initial

---
+++ function/main.js
@@ -0,0 +1,21 @@
+// const chao =function() {
+//     return 123;
+// }
+// var kq = chao();
+// alert(kq);
+
+// function square(x) { // tham so
+//     return x *x;
+// }
+// document.writeln("ket qua :"+ square(3));
+// function square() {
+//     return arguments;
+// }
+// var dsdoiso =square('hi',null,[1,2,3]);
+// console.log(dsdoiso);
+// function xinChao(name ='ban') {
+//     document.write(`xin chao ${name}`);
+// }
+// xinChao();
+var bp = (x) => x*x;
+document.write(bp(5));
